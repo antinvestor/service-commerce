ee3dd41305d1cc4d1c8b2c343c53601e75ebd108
Peter Bwire <bwire517@gmail.com>
2026-03-25 08:38:25 +0300

    feat: add buf.gen.yaml for local proto generation

---
+++ apps/default/service_commerce.opl.ts
@@ -0,0 +1,132 @@
+// Copyright 2023-2026 Ant Investor Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { Namespace, Context } from "@ory/keto-namespace-types"
+
+class profile_user implements Namespace {}
+
+class tenancy_access implements Namespace {
+  related: {
+    member: (profile_user | tenancy_access)[]
+    service: profile_user[]
+  }
+}
+
+class service_commerce implements Namespace {
+  related: {
+    owner: profile_user[]
+    admin: profile_user[]
+    operator: profile_user[]
+    viewer: profile_user[]
+    member: profile_user[]
+    service: (profile_user | tenancy_access)[]
+
+    granted_shop_view: (profile_user | service_commerce)[]
+    granted_shop_create: (profile_user | service_commerce)[]
+    granted_shop_update: (profile_user | service_commerce)[]
+    granted_product_view: (profile_user | service_commerce)[]
+    granted_product_manage: (profile_user | service_commerce)[]
+    granted_cart_view: (profile_user | service_commerce)[]
+    granted_cart_manage: (profile_user | service_commerce)[]
+    granted_order_view: (profile_user | service_commerce)[]
+    granted_order_manage: (profile_user | service_commerce)[]
+    granted_fulfilment_view: (profile_user | service_commerce)[]
+    granted_fulfilment_manage: (profile_user | service_commerce)[]
+  }
+
+  permits = {
+    shop_view: (ctx: Context): boolean =>
+      this.related.admin.includes(ctx.subject) ||
+      this.related.member.includes(ctx.subject) ||
+      this.related.operator.includes(ctx.subject) ||
+      this.related.owner.includes(ctx.subject) ||
+      this.related.service.includes(ctx.subject) ||
+      this.related.viewer.includes(ctx.subject) ||
+      this.related.granted_shop_view.includes(ctx.subject),
+
+    shop_create: (ctx: Context): boolean =>
+      this.related.admin.includes(ctx.subject) ||
+      this.related.owner.includes(ctx.subject) ||
+      this.related.service.includes(ctx.subject) ||
+      this.related.granted_shop_create.includes(ctx.subject),
+
+    shop_update: (ctx: Context): boolean =>
+      this.related.admin.includes(ctx.subject) ||
+      this.related.owner.includes(ctx.subject) ||
+      this.related.service.includes(ctx.subject) ||
+      this.related.granted_shop_update.includes(ctx.subject),
+
+    product_view: (ctx: Context): boolean =>
+      this.related.admin.includes(ctx.subject) ||
+      this.related.member.includes(ctx.subject) ||
+      this.related.operator.includes(ctx.subject) ||
+      this.related.owner.includes(ctx.subject) ||
+      this.related.service.includes(ctx.subject) ||
+      this.related.viewer.includes(ctx.subject) ||
+      this.related.granted_product_view.includes(ctx.subject),
+
+    product_manage: (ctx: Context): boolean =>
+      this.related.admin.includes(ctx.subject) ||
+      this.related.operator.includes(ctx.subject) ||
+      this.related.owner.includes(ctx.subject) ||
+      this.related.service.includes(ctx.subject) ||
+      this.related.granted_product_manage.includes(ctx.subject),
+
+    cart_view: (ctx: Context): boolean =>
+      this.related.admin.includes(ctx.subject) ||
+      this.related.member.includes(ctx.subject) ||
+      this.related.operator.includes(ctx.subject) ||
+      this.related.owner.includes(ctx.subject) ||
+      this.related.service.includes(ctx.subject) ||
+      this.related.viewer.includes(ctx.subject) ||
+      this.related.granted_cart_view.includes(ctx.subject),
+
+    cart_manage: (ctx: Context): boolean =>
+      this.related.admin.includes(ctx.subject) ||
+      this.related.owner.includes(ctx.subject) ||
+      this.related.service.includes(ctx.subject) ||
+      this.related.granted_cart_manage.includes(ctx.subject),
+
+    order_view: (ctx: Context): boolean =>
+      this.related.admin.includes(ctx.subject) ||
+      this.related.member.includes(ctx.subject) ||
+      this.related.operator.includes(ctx.subject) ||
+      this.related.owner.includes(ctx.subject) ||
+      this.related.service.includes(ctx.subject) ||
+      this.related.viewer.includes(ctx.subject) ||
+      this.related.granted_order_view.includes(ctx.subject),
+
+    order_manage: (ctx: Context): boolean =>
+      this.related.admin.includes(ctx.subject) ||
+      this.related.operator.includes(ctx.subject) ||
+      this.related.owner.includes(ctx.subject) ||
+      this.related.service.includes(ctx.subject) ||
+      this.related.granted_order_manage.includes(ctx.subject),
+
+    fulfilment_view: (ctx: Context): boolean =>
+      this.related.admin.includes(ctx.subject) ||
+      this.related.operator.includes(ctx.subject) ||
+      this.related.owner.includes(ctx.subject) ||
+      this.related.service.includes(ctx.subject) ||
+      this.related.viewer.includes(ctx.subject) ||
+      this.related.granted_fulfilment_view.includes(ctx.subject),
+
+    fulfilment_manage: (ctx: Context): boolean =>
+      this.related.admin.includes(ctx.subject) ||
+      this.related.operator.includes(ctx.subject) ||
+      this.related.owner.includes(ctx.subject) ||
+      this.related.service.includes(ctx.subject) ||
+      this.related.granted_fulfilment_manage.includes(ctx.subject),
+  }
+}
